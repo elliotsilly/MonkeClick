d5e20c2d7a4707624cfcf21b55654973724cb41c
elliot! <benjikrolfanpageuwu@gmail.com>
2025-06-21 12:23:15 +0100

    Update Plugin.cs
    Removed BananaOS functions, works through mod togglers now. Thinking about adding color settings when GorillaInfoWatch fully releases.

--- Plugin.cs
+++ Plugin.cs
@@ -1,30 +1,37 @@
-﻿using System.Text;
 using BepInEx;
 using GorillaLocomotion;
 using UnityEngine;
-using BananaOS.Pages;
-using BananaOS;
-using System;
-using System.Collections.Generic;
-
-//Yes, I'm an American I spell it "color" not "colour" go cry about it.
 
 namespace MonkeClick
 {
-        [BepInPlugin("baggz.monkeclick", "MonkeClick", "1.0.0")]
-        public class Plugin : BaseUnityPlugin
+    [BepInPlugin("baggz.monkeclick", "MonkeClick", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static LineRenderer lineRenderer;
+        public static bool isEnabled = false;
+
+        void OnEnable()
         {
-            bool inRoom;
-            public static LineRenderer lineRenderer;
+            isEnabled = true;
+            GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+            Debug.Log("MonkeClick enabled.");
+        }
 
-
-            void Start()
+        void OnDisable()
+        {
+            isEnabled = false;
+            if (lineRenderer != null)
             {
-                GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+                Destroy(lineRenderer.gameObject);
+                lineRenderer = null;
             }
+            Debug.Log("MonkeClick disabled.");
+        }
 
         void OnGameInitialized()
         {
+            if (!isEnabled) return;
+
             GameObject lineObj = new GameObject("RaycastLine");
             lineRenderer = lineObj.AddComponent<LineRenderer>();
 
@@ -32,109 +39,45 @@ namespace MonkeClick
             lineRenderer.startWidth = 0.01f;
             lineRenderer.endWidth = 0.01f;
             lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+            lineRenderer.startColor = Color.white;
+            lineRenderer.endColor = Color.white;
             lineRenderer.enabled = false;
         }
 
-        public static void RefreshColor(Color newColor, LineRenderer line, int newColorIndex)
+        void FixedUpdate()
         {
-            line.startColor = newColor;
-            line.endColor = newColor;
-        }
+            if (!isEnabled || lineRenderer == null) return;
 
-            void FixedUpdate()
+            lineRenderer.SetPosition(0, GTPlayer.Instance.rightControllerTransform.position);
+
+            if (ControllerInputPoller.instance.rightGrab)
             {
-                lineRenderer.SetPosition(0, GTPlayer.Instance.rightControllerTransform.position);
+                lineRenderer.enabled = true;
 
-                if (ControllerInputPoller.instance.rightGrab && Page.active)
+                var hits = Physics.RaycastAll(
+                    GTPlayer.Instance.rightControllerTransform.position,
+                    GTPlayer.Instance.rightControllerTransform.forward,
+                    Mathf.Infinity,
+                    ~(1 << LayerMask.NameToLayer("Gorilla Boundary")));
+
+                System.Array.Sort(hits, (a, b) => a.distance.CompareTo(b.distance));
+
+                foreach (var hit in hits)
                 {
-                    lineRenderer.enabled = true;
-                    
-                    var hits = Physics.RaycastAll(GTPlayer.Instance.rightControllerTransform.position, GTPlayer.Instance.rightControllerTransform.forward, Mathf.Infinity, ~(1 << LayerMask.NameToLayer("Gorilla Boundary")));
+                    lineRenderer.SetPosition(1, hit.point);
 
-                    System.Array.Sort(hits, (a, b) => a.distance.CompareTo(b.distance));
-
-                    foreach (var hit in hits)
+                    if (ControllerInputPoller.instance.rightControllerIndexFloat > 0.5f &&
+                        hit.transform.gameObject.layer == 18)
                     {
-                            lineRenderer.enabled = true;
-                            lineRenderer.SetPosition(1, hit.point);
-                            if (ControllerInputPoller.instance.rightControllerIndexFloat > 0.5f && hit.transform.gameObject.layer == 18)
-                            {
-                                GorillaTagger.Instance.rightHandTriggerCollider.transform.position = hit.point;
-                                break;
-                            }
+                        GorillaTagger.Instance.rightHandTriggerCollider.transform.position = hit.point;
+                        break;
                     }
                 }
-                else
-                {
-                    lineRenderer.enabled = false;
-                }
             }
-        }
-
-    public class Page : WatchPage
-    {
-        public override string Title => "Monke Click";
-        public static bool active = false;
-        public static int colorIndex = 0;
-        public static List<Tuple<string, Color>> colors = new List<Tuple<string, Color>>
-        {
-            Tuple.Create("white", Color.white),
-            Tuple.Create("red", Color.red),
-            Tuple.Create("green", Color.green),
-            Tuple.Create("blue", Color.blue),
-            Tuple.Create("yellow", Color.yellow),
-            Tuple.Create("cyan", Color.cyan),
-            Tuple.Create("magenta", Color.magenta),
-            Tuple.Create("black", Color.black)
-            //if you want more colors make a pull request with the colors you want.
-        };
-            public override bool DisplayOnMainMenu => true;
-
-            public override void OnPostModSetup()
+            else
             {
-                selectionHandler.maxIndex = 1;
-            }
-
-            public override string OnGetScreenContent()
-            {
-                var stringBuilder = new StringBuilder();
-                stringBuilder.AppendLine($"<color=yellow>==</color> Monke Click <color=yellow>==</color>");
-                stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(0, $"activity: {(active?"enabled":"disabled")}"));
-                stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(1, $"color: {colors[colorIndex].Item1}"));
-                return stringBuilder.ToString();
-            }
-
-            public override void OnButtonPressed(WatchButtonType buttonType)
-            {
-                switch (buttonType)
-                {
-                    case WatchButtonType.Up:
-                        selectionHandler.MoveSelectionUp();
-                        break;
-
-                    case WatchButtonType.Down:
-                        selectionHandler.MoveSelectionDown();
-                        break;
-
-                    case WatchButtonType.Enter:
-                    if (selectionHandler.currentIndex == 0) active = !active;
-                    else
-                    {
-                        colorIndex += 1;
-
-                        if (colorIndex > colors.Count - 1)
-                        {
-                            colorIndex = 0;
-                        }
-                        Plugin.RefreshColor(colors[colorIndex].Item2, Plugin.lineRenderer, colorIndex);
-                    }
-
-                        break;
-
-                    case WatchButtonType.Back:
-                        ReturnToMainMenu();
-                        break;
-                }
+                lineRenderer.enabled = false;
             }
         }
     }
+}
